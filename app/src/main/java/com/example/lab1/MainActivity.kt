311c5cfdb5cee1383ccf57b1fef80459a0926b51
Vladimir Panikarovskikh <panikar5437@gmail.com>
2023-02-27 19:40:54 +0500

    Lab1

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.lab1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import com.example.lab1.databinding.ActivityMainBinding
 
 class  MainActivity : AppCompatActivity() {
@@ -15,15 +14,8 @@ class  MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
         bindingClass.button1.setOnClickListener() {
-
-            if (bindingClass.button1.text == "Показать") {
-                bindingClass.button1.text = "Скрыть"
-                bindingClass.textView1.isVisible = true
-            }
-            else {
-                bindingClass.button1.text = "Показать"
-                bindingClass.textView1.isVisible = false
-            }
+            val text = bindingClass.textInput.text
+            bindingClass.textView1.text = text
         }
     }
 }
